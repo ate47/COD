c4dec24b4bd631cba42624743aa95393b6622403
ATE47 <wilate@yahoo.fr>
2021-10-19 09:46:20 +0200

    switch to v1 papi-client add multiple game usage

--- fetch_bo4_data.ps1
+++ fetch_bo4_data.ps1
@@ -28,9 +28,27 @@ param(
 	$Session
 )
 
-$Modes = @(
-	"mp", "blackout", "zombies"
-)
+if ("bo4" -eq $title) {
+	$Modes = @(
+		"mp", "blackout", "zombies"
+	)
+}
+elseif ("cw" -eq $title -or "bo3" -eq $title) {
+	$Modes = @(
+		"mp", "zombies"
+	)
+}
+elseif ("mw" -eq $title) {
+	$Modes = @(
+		"wz", "mp"
+	)
+}
+else {
+	$Modes = @(
+		"mp"
+	)
+}
+
 
 if ($null -eq $Session) {
 	$Session = ./build_sso_websession.ps1 -CookieDomain "my.callofduty.com"
@@ -45,7 +63,7 @@ New-Item -ItemType Directory "output_account/$title" -Force > $null
 
 foreach ($Mode in $Modes) {
 	$name = "output_account/$title/$($Platform)_$($Username)_$Mode.json"
-	$uri = "https://my.callofduty.com/api/papi-client/crm/cod/v2/title/$title/platform/$Platform/gamer/$UserName/profile/type/$Mode/"
+	$uri = "https://my.callofduty.com/api/papi-client/stats/cod/v1/title/$title/platform/$Platform/gamer/$UserName/profile/type/$Mode/"
 	Write-Host "Saving data $uri to $name..."
 	$json = (Invoke-WebRequest -Uri $uri -WebSession $Session).Content | ConvertFrom-Json -Depth 8
 	$json | ConvertTo-Json -Depth 8 | Out-File -Encoding utf8 $name > $null
