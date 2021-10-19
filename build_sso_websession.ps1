9f7b637f93852ce43edbcb96828a8fffe555d9a1
ATE47 <wilate@yahoo.fr>
2021-10-19 09:45:54 +0200

    remove useless cookies

--- build_sso_websession.ps1
+++ build_sso_websession.ps1
@@ -41,11 +41,13 @@ if ($null -eq $loginData) {
 				Write-Error "Can't read '$SaveFile', bad json?"
 				Exit -1
 			}
-		} else {
+		}
+		else {
 			Write-Error "'$SaveFile' doesn't exist or can't be read."
 			Exit -1
 		}
-	} else {
+	}
+ else {
 		Write-Error "`$loginData and `$SaveFile can't both be null."
 		Exit -1
 	}
@@ -61,9 +63,7 @@ $UserArgent = "cod-ate-worker"
 $CodSession = [Microsoft.PowerShell.Commands.WebRequestSession]::new()
 $CodSession.Headers.Add("Authorization", "bearer $authHeader")
 $CodSession.Headers.Add("X_COD_DEVICE_ID", $deviceId)
-$CodSession.Cookies.Add([System.Net.Cookie]::new("rtkn", $loginResponse.rtkn, "/", $CookieDomain))
-$CodSession.Cookies.Add([System.Net.Cookie]::new("ACT_SSO_COOKIE", $loginResponse.s_ACT_SSO_COOKIE, "/", $CookieDomain))
-$CodSession.Cookies.Add([System.Net.Cookie]::new("atkn", $loginResponse.atkn, "/", $CookieDomain))
+$CodSession.Cookies.Add([System.Net.Cookie]::new("ACT_SSO_COOKIE", $loginResponse.ACT_SSO_COOKIE, "/", $CookieDomain))
 $CodSession.UserAgent = $UserArgent
 
 return $CodSession
